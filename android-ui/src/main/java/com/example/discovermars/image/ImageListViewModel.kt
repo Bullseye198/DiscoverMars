6d2ea94eb67f4783cb39b1d50089d3fdde9780e8
Bullseye198 <martin.vojko@gmail.com>
2020-04-30 10:25:39 +0200

    Switching between fragments works

--- android-ui/src/main/java/com/example/discovermars/image/ImageListViewModel.kt
+++ android-ui/src/main/java/com/example/discovermars/image/ImageListViewModel.kt
@@ -11,9 +11,14 @@ import javax.inject.Inject
 class ImageListViewModel @Inject constructor(
 ) : ViewModel() {
 
-    private val changeImageState = MutableLiveData<List<Image>>()
-    val noteList: LiveData<List<Image>> get() = changeImageState
+    private val imageListState = MutableLiveData<List<Image>>()
+    val imageList: LiveData<List<Image>> get() = imageListState
 
+    private val changeImageState = MutableLiveData<String>()
+    val editImage: LiveData<String> get() = changeImageState
+
+
+//TODO: Check out this handle event and use it.
      fun handleEvent(event: ImageListEvent) {
         when(event) {
             is ImageListEvent.OnImageItemClick -> getImageDetail(event.position)
@@ -28,6 +33,6 @@ class ImageListViewModel @Inject constructor(
     }
 
     private fun getImageDetail(position: Int) {
-
+    changeImageState.value = imageList.value!![position].creationDate
     }
 }
