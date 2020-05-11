a30ee69f3527fa1ebb83325fd24b2df962d838c4
Bullseye198 <martin.vojko@gmail.com>
2020-05-11 21:35:06 +0200

    Unsure it this is right(RequestImagesUseCase)

--- domain/src/main/java/com/example/domain/usecases/RequestImagesUseCase.kt
+++ domain/src/main/java/com/example/domain/usecases/RequestImagesUseCase.kt
@@ -1,19 +1,30 @@
 package com.example.domain.usecases
 
 import com.cm.base.executor.AppCoroutineDispatchers
+import com.cm.base.executor.AppRxSchedulers
 import com.example.domain.image.IImageRepository
 import com.example.domain.image.model.Image
+import io.reactivex.subscribers.DisposableSubscriber
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RequestImagesUseCase @Inject constructor(
     private val iImageRepository: IImageRepository,
-    val appDispatchers: AppCoroutineDispatchers
-
+    val appDispatchers: AppCoroutineDispatchers,
+    private val rxSchedulers: AppRxSchedulers
 ) {
 
-    suspend fun requestImages(earthDate: String? = null, camera: String? = null): List<Image> {
-        return withContext(appDispatchers.io) { iImageRepository.requestImages(earthDate, camera) }
+     fun requestImages(nina: DisposableSubscriber<Image>,
+                       earthDate: String? = null,
+                       camera: String? = null) {
+       // return withContext(appDispatchers.io) { iImageRepository.requestImages(earthDate, camera) }
+         iImageRepository.observeImages()
+             .map { images -> images.first { it.creationDate == earthDate &&
+                     it.camera == camera} }
+             .subscribeOn(rxSchedulers.io)
+             .observeOn(rxSchedulers.main)
+             .subscribeWith(nina)
+
     }
 
 }
